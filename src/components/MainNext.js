bd6b36e4a8d7ef167193f44b23b0dc2a99dd59e3
EniolaOluwa <eebakare@gmail.com>
2022-11-15 11:37:18 +0100

    I completed the stylingfor the hero section at the bottom of the landing page

--- src/components/MainNext.js
+++ src/components/MainNext.js
@@ -3,10 +3,10 @@ import React from 'react'
 
 const MainNext = () => {
   return (
-    <div>
+    <div className='main-next'>
       <div className="main-text">
-        <Typography variant='body' component='h1'>Metabnb NFTs</Typography>
-        <Typography variant='body'>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.</Typography>
+        <Typography variant='body' component='h1' className='main-title'>Metabnb NFTs</Typography>
+        <Typography variant='body' className='main-body'>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.</Typography>
         <button>Learn more </button>
       </div>
       <div className="main-image">
